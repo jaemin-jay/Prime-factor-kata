333014706ff15e98b4c93f20764f168b2ef2aaca
Jaemin Choi <jaemin.jay@gmail.com>
2025-04-11 13:57:28 +0900

    [feature] page 37

--- day8_kata/prime-factor.cpp
+++ day8_kata/prime-factor.cpp
@@ -2,9 +2,12 @@
 
 class PrimeFactor {
 public:
-	std::vector<int> of(int i) {
+	std::vector<int> of(int number) {
 		std::vector<int> result = {};
 
+		if (number == 2) {
+			result.push_back(2);
+		}
 		return result;
 	}
 };
