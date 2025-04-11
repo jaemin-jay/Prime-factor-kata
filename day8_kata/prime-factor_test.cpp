cf96810f18485e6e3a6c73b3e591b5e55766e25d
Jaemin Choi <jaemin.jay@gmail.com>
2025-04-11 14:18:53 +0900

    [feature] page 58

--- day8_kata/prime-factor_test.cpp
+++ day8_kata/prime-factor_test.cpp
@@ -45,6 +45,12 @@ TEST_F(PrimeFixture, Of9) {
 	EXPECT_EQ(expected, prime_factor.of(9));
 }
 
+TEST_F(PrimeFixture, Of12) {
+	expected = { 2, 2, 3 };
+
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
