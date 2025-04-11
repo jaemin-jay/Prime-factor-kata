bd87c9f0323b952bf61463b93e81575017968cca
Jaemin Choi <jaemin.jay@gmail.com>
2025-04-11 14:00:53 +0900

    [refactoring] page 42

--- day8_kata/prime-factor.cpp
+++ day8_kata/prime-factor.cpp
@@ -5,12 +5,8 @@ public:
 	std::vector<int> of(int number) {
 		std::vector<int> result = {};
 
-		if (number == 2) {
-			result.push_back(2);
-		}
-
-		if (number == 3) {
-			result.push_back(3);
+		if (number > 1) {
+			result.push_back(number);
 		}
 
 		return result;
